0740468b5f697285f66d2a9ce502670274ee0f89
heldrosil <heldersilv@pm.me>
2022-12-27 11:58:12 +0000

    bullhead: blobs from mtc20k factory image

---
+++ vendor/lge/bullhead/device-vendor.mk
@@ -0,0 +1,48 @@
+# [2022-12-27] Auto-generated file, do not edit
+
+VENDOR_DEVICE := $(TARGET_PRODUCT:aosp_%=%)
+$(call inherit-product, vendor/lge/$(VENDOR_DEVICE)/$(VENDOR_DEVICE)-vendor-blobs.mk)
+
+# Prebuilt APKs/JARs from 'vendor/app'
+PRODUCT_PACKAGES += \
+    datastatusnotification \
+    ims
+
+# Prebuilt APKs libs symlinks from 'vendor/app'
+PRODUCT_PACKAGES += \
+    libimscamera_jni_64.so \
+    libimsmedia_jni_64.so
+
+# Prebuilt APKs/JARs from 'proprietary/app'
+PRODUCT_PACKAGES += \
+    atfwd \
+    HiddenMenu \
+    qcrilmsgtunnel \
+    RCSBootstraputil \
+    RcsImsBootstraputil \
+    TimeService
+
+# Prebuilt APKs/JARs from 'proprietary/framework'
+PRODUCT_PACKAGES += \
+    cneapiclient \
+    com.google.widevine.software.drm \
+    qcrilhook \
+    rcsimssettings \
+    rcsservice
+
+# Prebuilt APKs/JARs from 'proprietary/priv-app'
+PRODUCT_PACKAGES += \
+    CNEService \
+    ConnMO \
+    DCMO \
+    DiagMon \
+    DMConfigUpdate \
+    DMService \
+    LifeTimerService \
+    SprintDM
+
+# Prebuilt APKs libs symlinks from 'proprietary/priv-app'
+PRODUCT_PACKAGES += \
+    libdmjavaplugin_32.so \
+    libdmengine_32.so
+
