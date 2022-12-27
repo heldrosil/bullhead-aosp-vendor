204f137241255496ec2c9b989f8894b234de5965
heldrosil <heldersilv@pm.me>
2022-12-27 12:01:06 +0000

    bullhead: blobs from n2g48c factory image

---
+++ vendor/lge/bullhead/device-vendor.mk
@@ -0,0 +1,74 @@
+# [2022-12-27] Auto-generated file, do not edit
+
+VENDOR_DEVICE := $(TARGET_PRODUCT:aosp_%=%)
+$(call inherit-product, vendor/lge/$(VENDOR_DEVICE)/$(VENDOR_DEVICE)-vendor-blobs.mk)
+
+# Prebuilt APKs/JARs from 'vendor/app'
+PRODUCT_PACKAGES += \
+    datastatusnotification \
+    ims
+
+# Prebuilt APKs libs symlinks from 'vendor/app'
+PRODUCT_PACKAGES += \
+    libimscamera_jni_64.so \
+    libimsmedia_jni_64.so
+
+# Prebuilt APKs/JARs from 'proprietary/app'
+PRODUCT_PACKAGES += \
+    RCSBootstraputil \
+    RcsImsBootstraputil \
+    TimeService \
+    Tycho
+
+# Prebuilt APKs/JARs from 'proprietary/framework'
+PRODUCT_PACKAGES += \
+    cneapiclient \
+    com.google.widevine.software.drm \
+    qcrilhook \
+    rcsimssettings \
+    rcsservice
+
+# Prebuilt APKs/JARs from 'proprietary/priv-app'
+PRODUCT_PACKAGES += \
+    atfwd \
+    CarrierEntitlement \
+    CNEService \
+    ConnMO \
+    DCMO \
+    DiagMon \
+    DMConfigUpdate \
+    DMService \
+    Entitlement \
+    GCS \
+    LifeTimerService \
+    qcrilmsgtunnel \
+    SprintDM
+
+# Prebuilt APKs libs symlinks from 'proprietary/priv-app'
+PRODUCT_PACKAGES += \
+    libdmjavaplugin_32.so \
+    libdmengine_32.so
+
+# Standalone symbolic links
+PRODUCT_PACKAGES += \
+    eglSubDriverAndroid_64.so__eglSubDriverAndroid.so \
+    libEGL_adreno_64.so__libEGL_adreno.so \
+    libGLESv1_CM_adreno_64.so__libGLESv1_CM_adreno.so \
+    libGLESv2_adreno_64.so__libGLESv2_adreno.so \
+    libq3dtools_adreno_64.so__libq3dtools_adreno.so \
+    libq3dtools_esx_64.so__libq3dtools_esx.so
+
+# Prebuilt shared libraries
+PRODUCT_PACKAGES += \
+    libloc_api_v02 \
+    libloc_ds_api \
+    libdsi_netctrl \
+    libqmi_cci \
+    libqmi_common_so \
+    libqmi_csi \
+    libqmiservices
+
+# Extra modules from user configuration
+PRODUCT_PACKAGES += \
+    ETC_Bullhead_AOSPLinks
+
