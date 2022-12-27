f3530079c4608c4e68a7c0a8c4960eda20d86da8
heldrosil <heldersilv@pm.me>
2022-12-27 12:03:59 +0000

    bullhead: blobs from opm7.181205.001 factory image

---
+++ vendor/lge/bullhead/AndroidBoardVendor.mk
@@ -0,0 +1,6 @@
+# [2022-12-27] Auto-generated file, do not edit
+
+LOCAL_PATH := $(call my-dir)
+
+$(call add-radio-file,radio/bootloader.img,version-bootloader)
+$(call add-radio-file,radio/radio.img,version-baseband)
